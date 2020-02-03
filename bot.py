dcfb0616b8d9591b09032726b1f14b750f74212b
max <mak2001sim21@mail.ru>
2020-02-03 18:59:42 +0100

    init commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import telebot
+
+class Doc:
+    def __init__(self, name, id, user, description, folder):
+        self.name = name
+        self.id = id
+        self.user = user
+        self.description = description
+        self.folder = folder
+
+
+class Doc:
+    def __init__(self, name, id, user, description, folder):
+        self.name = name
+        self.id = id
+        self.user = user
+        self.description = description
+        self.folder = folder
+
